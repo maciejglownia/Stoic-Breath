cd6e5e815acd95ceb6c92c1e54f874fea63ecf27
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-01 11:44:32 +0200

    Create Dao interface

---
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/data/QuoteDao.kt
@@ -0,0 +1,24 @@
+package pl.glownia.maciej.stoicbreath.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import pl.glownia.maciej.stoicbreath.Quote
+
+@Dao
+interface QuoteDao {
+
+    @Query("SELECT * FROM quotes")
+    fun getAllQuotes(): Flow<List<Quote>>
+
+    @Query("SELECT * FROM quotes WHERE id = :id")
+    fun getQuoteById(id: Int): Flow<Quote>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(quoteList: List<Quote>)
+
+    @Update
+    suspend fun update(quote: Quote)
+
+    @Query("DELETE FROM quotes")
+    suspend fun clearQuotes()
+}
