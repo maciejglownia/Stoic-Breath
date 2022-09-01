3762e79fa3c664b48e65f8a79e6e82e1e456f4a2
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-01 12:21:05 +0200

    Create Database

---
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/data/QuoteDatabase.kt
@@ -0,0 +1,35 @@
+package pl.glownia.maciej.stoicbreath.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import pl.glownia.maciej.stoicbreath.Quote
+
+@Database(
+    entities = [Quote::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class QuoteDatabase : RoomDatabase() {
+
+    abstract fun quoteDao(): QuoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: QuoteDatabase? = null
+        fun getDatabase(context: Context): QuoteDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    QuoteDatabase::class.java,
+                    "quotes_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
