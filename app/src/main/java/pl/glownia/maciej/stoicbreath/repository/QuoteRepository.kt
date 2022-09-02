96ed51feeca58a6c407fdfe645863b85d7f8791b
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-02 15:27:12 +0200

    Add function in repository to get quotes

--- app/src/main/java/pl/glownia/maciej/stoicbreath/repository/QuoteRepository.kt
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/repository/QuoteRepository.kt
@@ -1,8 +1,10 @@
 package pl.glownia.maciej.stoicbreath.repository
 
+import pl.glownia.maciej.stoicbreath.api.RetrofitInstance
 import pl.glownia.maciej.stoicbreath.data.QuoteDatabase
 
 class QuoteRepository(
     val database: QuoteDatabase
 ) {
+    suspend fun getQuotes() = RetrofitInstance.api.getQuotes()
 }
