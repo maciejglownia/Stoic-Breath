686026ad3219a699b43c5eb9f6f9460cb2d6b472
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-02 15:25:51 +0200

    Update QuoteListViewModelProviderFactory after changes

--- app/src/main/java/pl/glownia/maciej/stoicbreath/ui/QuoteListViewModelProviderFactory.kt
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/ui/QuoteListViewModelProviderFactory.kt
@@ -2,12 +2,18 @@ package pl.glownia.maciej.stoicbreath.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import pl.glownia.maciej.stoicbreath.data.QuoteDao
 import pl.glownia.maciej.stoicbreath.repository.QuoteRepository
 
 class QuoteListViewModelProviderFactory(
-    val quoteRepository: QuoteRepository
+    val quoteDao: QuoteDao,
+    val repository: QuoteRepository
 ): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return QuoteListViewModel(quoteRepository) as T
+        if (modelClass.isAssignableFrom(QuoteListViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+        return QuoteListViewModel(quoteDao, repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
