5342043259604b6838024b0c09c48259e577df90
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-02 15:29:47 +0200

    Implement QuoteListFragment to display quotes from database

--- app/src/main/java/pl/glownia/maciej/stoicbreath/ui/fragments/QuoteListFragment.kt
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/ui/fragments/QuoteListFragment.kt
@@ -1,7 +1,54 @@
 package pl.glownia.maciej.stoicbreath.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import pl.glownia.maciej.stoicbreath.R
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import pl.glownia.maciej.stoicbreath.QuoteApplication
+import pl.glownia.maciej.stoicbreath.adapters.QuoteListAdapter
+import pl.glownia.maciej.stoicbreath.data.QuoteDatabase
+import pl.glownia.maciej.stoicbreath.databinding.FragmentQuoteListBinding
+import pl.glownia.maciej.stoicbreath.repository.QuoteRepository
+import pl.glownia.maciej.stoicbreath.ui.QuoteListViewModel
+import pl.glownia.maciej.stoicbreath.ui.QuoteListViewModelProviderFactory
 
-class QuoteListFragment : Fragment(R.layout.fragment_quote_list) {
+class QuoteListFragment : Fragment() {
+
+    private val viewModel: QuoteListViewModel by activityViewModels() {
+        QuoteListViewModelProviderFactory(
+            (activity?.application as QuoteApplication).database.quoteDao(),
+            QuoteRepository(QuoteDatabase.getDatabase(requireContext().applicationContext))
+        )
+    }
+
+    private var _binding: FragmentQuoteListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentQuoteListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = QuoteListAdapter {
+            val action = QuoteListFragmentDirections.actionQuoteListFragmentToSingleQuoteFragment()
+            this.findNavController().navigate(action)
+        }
+        viewModel.quotes.observe(this.viewLifecycleOwner) { quotes ->
+            quotes.let {
+                adapter.submitList(it)
+            }
+        }
+        binding.rvQuoteList.adapter = adapter
+        binding.rvQuoteList.layoutManager = LinearLayoutManager(this.context)
+    }
 }
