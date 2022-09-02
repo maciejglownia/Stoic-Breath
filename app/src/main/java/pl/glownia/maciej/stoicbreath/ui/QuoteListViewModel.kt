9e1fb084e12b896199f9c3f4bd87e94284b70267
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-02 15:25:04 +0200

    Implement QuoteViewModel to get data from api and insert it to database

--- app/src/main/java/pl/glownia/maciej/stoicbreath/ui/QuoteListViewModel.kt
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/ui/QuoteListViewModel.kt
@@ -1,9 +1,31 @@
 package pl.glownia.maciej.stoicbreath.ui
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import pl.glownia.maciej.stoicbreath.data.QuoteDao
+import pl.glownia.maciej.stoicbreath.models.Quote
 import pl.glownia.maciej.stoicbreath.repository.QuoteRepository
 
 class QuoteListViewModel(
-    val quoteRepository: QuoteRepository
-): ViewModel() {
+    private val quoteDao: QuoteDao,
+    repository: QuoteRepository,
+) : ViewModel() {
+
+    private val quoteRepository: QuoteRepository = repository
+    val quotes: LiveData<List<Quote>> = quoteDao.getAllQuotes().asLiveData()
+
+    init {
+        getQuotes()
+    }
+
+    private fun getQuotes() {
+        viewModelScope.launch {
+            val response = quoteRepository.getQuotes()
+            quoteDao.clearQuotes()
+            quoteDao.insertAll(response)
+        }
+    }
 }
