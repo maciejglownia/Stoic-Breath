e315cc30d6cc7ef9b8fd92b27fa72e3e4e337e82
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-03 17:13:07 +0200

    Add try catch block to getQuotes method in QuoteListViewModel

--- app/src/main/java/pl/glownia/maciej/stoicbreath/ui/QuoteListViewModel.kt
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/ui/QuoteListViewModel.kt
@@ -1,5 +1,6 @@
 package pl.glownia.maciej.stoicbreath.ui
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
@@ -8,6 +9,8 @@ import kotlinx.coroutines.launch
 import pl.glownia.maciej.stoicbreath.data.QuoteDao
 import pl.glownia.maciej.stoicbreath.models.Quote
 import pl.glownia.maciej.stoicbreath.repository.QuoteRepository
+import retrofit2.HttpException
+import java.io.IOException
 
 class QuoteListViewModel(
     private val quoteDao: QuoteDao,
@@ -23,9 +26,18 @@ class QuoteListViewModel(
 
     private fun getQuotes() {
         viewModelScope.launch {
-            val response = quoteRepository.getQuotes()
-            quoteDao.clearQuotes()
-            quoteDao.insertAll(response)
+            try {
+                Log.e("QuoteListViewModel", "getQuotes: Getting quotes from Api...")
+                val response = quoteRepository.getQuotes()
+                quoteDao.clearQuotes()
+                quoteDao.insertAll(response)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Log.e("QuoteListViewModel", "getQuotes: IOException")
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                Log.e("QuoteListViewModel", "getQuotes: HttpException")
+            }
         }
     }
 }
