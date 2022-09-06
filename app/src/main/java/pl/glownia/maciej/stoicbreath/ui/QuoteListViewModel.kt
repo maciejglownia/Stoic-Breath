4aeae86d8eb6190e445b7af4e1b74ac4b0e6d2cc
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-06 12:35:46 +0200

    Implement functionality to get data from api only when database is empty

--- app/src/main/java/pl/glownia/maciej/stoicbreath/ui/QuoteListViewModel.kt
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/ui/QuoteListViewModel.kt
@@ -12,32 +12,44 @@ import pl.glownia.maciej.stoicbreath.repository.QuoteRepository
 import retrofit2.HttpException
 import java.io.IOException
 
+
 class QuoteListViewModel(
     private val quoteDao: QuoteDao,
     repository: QuoteRepository,
 ) : ViewModel() {
 
     private val quoteRepository: QuoteRepository = repository
-    val quotes: LiveData<List<Quote>> = quoteDao.getAllQuotes().asLiveData()
 
     init {
-        getQuotes()
+        getQuotesFromApi()
     }
 
-    private fun getQuotes() {
-        viewModelScope.launch {
-            try {
-                Log.e("QuoteListViewModel", "getQuotes: Getting quotes from Api...")
-                val response = quoteRepository.getQuotes()
-                quoteDao.clearQuotes()
-                quoteDao.insertAll(response)
-            } catch (e: IOException) {
-                e.printStackTrace()
-                Log.e("QuoteListViewModel", "getQuotes: IOException")
-            } catch (e: HttpException) {
-                e.printStackTrace()
-                Log.e("QuoteListViewModel", "getQuotes: HttpException")
+    private fun getQuotesFromApi() {
+        Log.e("QuoteListViewModel","getQuotesFromApi: Is checking if database contains any data...")
+        val sizeOfTable = quoteRepository.getSizeOfTable()
+        if (sizeOfTable == 0) {
+            viewModelScope.launch {
+
+                Log.e(
+                    "QuoteListViewModel",
+                    "getQuotesFromApi: Database is empty so it is going to fetch data from Api"
+                )
+                try {
+                    Log.e("QuoteListViewModel", "getQuotesFromApi: Getting quotes from Api...")
+                    val response = quoteRepository.getQuotesFromApi()
+                    Log.e("QuoteListViewModel", "getQuotesFromApi: Clearing database...")
+                    quoteDao.clearQuotes()
+                    Log.e("QuoteListViewModel", "getQuotesFromApi: Inserting data...")
+                    quoteDao.insertAll(response)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    Log.e("QuoteListViewModel", "getQuotesFromApi: IOException")
+                } catch (e: HttpException) {
+                    e.printStackTrace()
+                    Log.e("QuoteListViewModel", "getQuotesFromApi: HttpException")
+                }
             }
         }
+        Log.e("QuoteListViewModel", "getQuotesFromApi: Database already has data")
     }
 }
