098d096643d185f43773bbe681a89a704da74c24
Maciej Glownia <maciej.k.glownia@gmail.com>
2022-09-06 12:34:01 +0200

    Add functionality to check if database contains any data

--- app/src/main/java/pl/glownia/maciej/stoicbreath/data/QuoteDao.kt
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/data/QuoteDao.kt
@@ -8,10 +8,14 @@ import pl.glownia.maciej.stoicbreath.models.Quote
 interface QuoteDao {
 
     @Query("SELECT * FROM quotes")
-    fun getAllQuotes(): Flow<List<Quote>>
+    fun getQuotesFromDatabase(): Flow<List<Quote>>
 
     @Query("SELECT * FROM quotes WHERE id = :id")
     fun getQuoteById(id: Int): Flow<Quote>
+
+    // Needed to check if database contains any data
+    @Query("SELECT COUNT(id) FROM quotes")
+    suspend fun getSizeOfTable(): Int
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(quoteList: List<Quote>)
--- app/src/main/java/pl/glownia/maciej/stoicbreath/repository/QuoteRepository.kt
+++ app/src/main/java/pl/glownia/maciej/stoicbreath/repository/QuoteRepository.kt
@@ -1,10 +1,22 @@
 package pl.glownia.maciej.stoicbreath.repository
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import pl.glownia.maciej.stoicbreath.api.RetrofitInstance
 import pl.glownia.maciej.stoicbreath.data.QuoteDatabase
 
 class QuoteRepository(
     val database: QuoteDatabase
 ) {
-    suspend fun getQuotes() = RetrofitInstance.api.getQuotes()
+    suspend fun getQuotesFromApi() = RetrofitInstance.api.getQuotes()
+
+    suspend fun getQuoteById(id: Int) = database.quoteDao().getQuoteById(id)
+
+    fun getSizeOfTable(): Int = runBlocking {
+        val count = async {
+            database.quoteDao().getSizeOfTable()
+        }
+        count.start()
+        count.await()
+    }
 }
